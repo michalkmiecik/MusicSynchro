0c7345fa74a2eae204ce1c047c93a39fb9f2f904
Michał Kmiecik <michal.dryman@gmail.com>
2019-03-04 17:54:27 +0100

    initial commit

---
+++ MusicSynchro/MainApp.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MusicSynchro
+{
+    partial class MainApp
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbSender = new System.Windows.Forms.RadioButton();
+            this.rbReciever = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStartWaiting = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Adress";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSend.Location = new System.Drawing.Point(3, 80);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(291, 38);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbIPAddress.Location = new System.Drawing.Point(86, 9);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(208, 22);
+            this.tbIPAddress.TabIndex = 11;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbPort.Location = new System.Drawing.Point(86, 43);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(208, 22);
+            this.tbPort.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(48, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Port";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rbSender
+            // 
+            this.rbSender.AutoSize = true;
+            this.rbSender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbSender.Location = new System.Drawing.Point(33, 22);
+            this.rbSender.Name = "rbSender";
+            this.rbSender.Size = new System.Drawing.Size(72, 21);
+            this.rbSender.TabIndex = 0;
+            this.rbSender.Text = "Sender";
+            this.rbSender.UseVisualStyleBackColor = true;
+            this.rbSender.CheckedChanged += new System.EventHandler(this.SenderOrReciever);
+            // 
+            // rbReciever
+            // 
+            this.rbReciever.AutoSize = true;
+            this.rbReciever.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbReciever.Location = new System.Drawing.Point(33, 49);
+            this.rbReciever.Name = "rbReciever";
+            this.rbReciever.Size = new System.Drawing.Size(82, 21);
+            this.rbReciever.TabIndex = 1;
+            this.rbReciever.Text = "Reciever";
+            this.rbReciever.UseVisualStyleBackColor = true;
+            this.rbReciever.CheckedChanged += new System.EventHandler(this.SenderOrReciever);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnSend);
+            this.panel2.Controls.Add(this.tbIPAddress);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.tbPort);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(10, 139);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(297, 126);
+            this.panel2.TabIndex = 16;
+            // 
+            // btnStartWaiting
+            // 
+            this.btnStartWaiting.Enabled = false;
+            this.btnStartWaiting.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStartWaiting.Location = new System.Drawing.Point(10, 94);
+            this.btnStartWaiting.Name = "btnStartWaiting";
+            this.btnStartWaiting.Size = new System.Drawing.Size(297, 38);
+            this.btnStartWaiting.TabIndex = 14;
+            this.btnStartWaiting.Text = "Start waiting";
+            this.btnStartWaiting.UseVisualStyleBackColor = true;
+            this.btnStartWaiting.Click += new System.EventHandler(this.btnStartWaiting_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbSender);
+            this.groupBox1.Controls.Add(this.rbReciever);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 78);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select your role";
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 275);
+            this.Controls.Add(this.btnStartWaiting);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainApp";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Synchro Player";
+            this.Load += new System.EventHandler(this.MainApp_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbReciever;
+        private System.Windows.Forms.RadioButton rbSender;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStartWaiting;
+    }
+}
+
---
+++ MusicSynchro/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicSynchro {
+    public partial class MainApp : Form {
+        UdpClient udpClient;
+
+        public MainApp() {
+            InitializeComponent();
+        }
+
+        private void MainApp_Load(object sender, EventArgs e) {
+            udpClient = new UdpClient();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e) {
+            try {
+                IPAddress myIP = IPAddress.Parse(tbIPAddress.Text);
+                IPEndPoint myEndPoint = new IPEndPoint(myIP, Int32.Parse(tbPort.Text));
+                udpClient.Connect(myEndPoint);
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Press space");
+
+                udpClient.Send(sendBytes, sendBytes.Length);
+
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                // Blocks until a message returns on this socket from a remote host.
+
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+
+                udpClient.Close();
+            }
+            catch (Exception exc) {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+
+        private void btnStartWaiting_Click(object sender, EventArgs e) {
+
+        }
+
+        private void SenderOrReciever(object sender, EventArgs e) {
+            if (rbReciever.Checked) {
+                btnStartWaiting.Enabled = true;
+                panel2.Enabled = false;
+            }
+            else {
+                panel2.Enabled = true;
+                btnStartWaiting.Enabled = false;
+            }
+        }
+
+    }
+}
